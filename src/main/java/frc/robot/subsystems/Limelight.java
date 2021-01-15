792577b9669ac03b2d9651ad995944860da63522
SatvikR <49799352+SatvikR@users.noreply.github.com>
2021-01-14 17:16:05 -0800

    Some stuff with lime colored lights

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * The Limelight subsystem is a light that is lime green. If you look at it at a
+ * certain angle, you will go blind, so read this code with caution.
+ */
+public class Limelight extends SubsystemBase {
+  private final NetworkTable limeTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+  private final NetworkTableEntry txEntry = limeTable.getEntry("tx");
+  private final NetworkTableEntry tyEntry = limeTable.getEntry("ty");
+  private final NetworkTableEntry tvEntry = limeTable.getEntry("tv");
+  private final NetworkTableEntry taEntry = limeTable.getEntry("ta");
+
+  public double tx;
+  public double ty;
+  public boolean tv;
+  public double ta;
+
+  @Override
+  public void periodic() {
+    tx = txEntry.getDouble(0);
+    ty = tyEntry.getDouble(0);
+    tv = tvEntry.getDouble(0) == 1;
+    ta = taEntry.getDouble(0);
+  }
+}
