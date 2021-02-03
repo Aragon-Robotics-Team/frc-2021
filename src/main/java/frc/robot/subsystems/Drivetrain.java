021cd6b847cfaddb4f61895c3fbe62b2e2f28a4d
kiran swami <kiran.m.swami@gmail.com>
2021-02-02 16:57:15 -0800

    ready to drive

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -31,11 +31,13 @@ public class Drivetrain extends SubsystemBase {
 
     final DifferentialDrive m_drive = new DifferentialDrive(leftMotorMaster, rightMotorMaster);
 
-    private final Encoder m_leftEncoder = new Encoder(Constants.kLeftEncoderPorts[0], Constants.kLeftEncoderPorts[1],
-            Constants.kLeftEncoderReversed);
+    // private final Encoder m_leftEncoder = new
+    // Encoder(Constants.kLeftEncoderPorts[0], Constants.kLeftEncoderPorts[1],
+    // Constants.kLeftEncoderReversed);
 
-    private final Encoder m_rightEncoder = new Encoder(Constants.kRightEncoderPorts[0], Constants.kRightEncoderPorts[1],
-            Constants.kRightEncoderReversed);
+    // private final Encoder m_rightEncoder = new
+    // Encoder(Constants.kRightEncoderPorts[0], Constants.kRightEncoderPorts[1],
+    // Constants.kRightEncoderReversed);
 
     private final Gyro m_gyro = new ADXRS450_Gyro();
 
@@ -49,10 +51,10 @@ public class Drivetrain extends SubsystemBase {
         leftMotorSlave.setInverted(true);
         leftMotorMaster.setInverted(true);
 
-        m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
-        m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+        // m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+        // m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
 
-        resetEncoder();
+        // resetEncoder();
         m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
 
     }
@@ -65,74 +67,76 @@ public class Drivetrain extends SubsystemBase {
         rightMotorMaster.set(speed);
     }
 
-    @Override
-    public void periodic() {
-        // Update the odometry in the periodic block
-        m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
-    }
+    // @Override
+    // public void periodic() {
+    // // Update the odometry in the periodic block
+    // m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(),
+    // m_rightEncoder.getDistance());
+    // }
 
-    public Pose2d getPose() {
-        return m_odometry.getPoseMeters();
-    }
+    // public Pose2d getPose() {
+    // return m_odometry.getPoseMeters();
+    // }
 
-    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-        return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
-    }
+    // public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+    // return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(),
+    // m_rightEncoder.getRate());
+    // }
 
-    public void resetEncoders() {
-        m_leftEncoder.reset();
-        m_rightEncoder.reset();
-    }
+    // public void resetEncoders() {
+    // m_leftEncoder.reset();
+    // m_rightEncoder.reset();
+    // }
 
-    public void resetOdometry(Pose2d pose) {
-        resetEncoders();
-        m_odometry.resetPosition(pose, m_gyro.getRotation2d());
-    }
+    // public void resetOdometry(Pose2d pose) {
+    // resetEncoders();
+    // m_odometry.resetPosition(pose, m_gyro.getRotation2d());
+    // }
 
-    public void arcadeDrive(double fwd, double rot) {
-        m_drive.arcadeDrive(fwd, rot);
-    }
+    // public void arcadeDrive(double fwd, double rot) {
+    // m_drive.arcadeDrive(fwd, rot);
+    // }
 
-    public void tankDriveVolts(double leftVolts, double rightVolts) {
-        m_leftMotors.setVoltage(leftVolts);
-        m_rightMotors.setVoltage(-rightVolts);
-        m_drive.feed();
-    }
+    // public void tankDriveVolts(double leftVolts, double rightVolts) {
+    // m_leftMotors.setVoltage(leftVolts);
+    // m_rightMotors.setVoltage(-rightVolts);
+    // m_drive.feed();
+    // }
 
-    public double getAverageEncoderDistance() {
-        return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
-    }
+    // public double getAverageEncoderDistance() {
+    // return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
+    // }
 
-    public Encoder getLeftEncoder() {
-        return m_leftEncoder;
-    }
+    // public Encoder getLeftEncoder() {
+    // return m_leftEncoder;
+    // }
 
-    public Encoder getRightEncoder() {
-        return m_rightEncoder;
-    }
+    // public Encoder getRightEncoder() {
+    // return m_rightEncoder;
+    // }
 
-    public void setMaxOutput(double maxOutput) {
-        m_drive.setMaxOutput(maxOutput);
-    }
+    // public void setMaxOutput(double maxOutput) {
+    // m_drive.setMaxOutput(maxOutput);
+    // }
 
-    public void zeroHeading() {
-        m_gyro.reset();
-    }
+    // public void zeroHeading() {
+    // m_gyro.reset();
+    // }
 
-    public double getHeading() {
-        return m_gyro.getRotation2d().getDegrees();
-    }
+    // public double getHeading() {
+    // return m_gyro.getRotation2d().getDegrees();
+    // }
 
-    public double getTurnRate() {
-        return -m_gyro.getRate();
-    }
+    // public double getTurnRate() {
+    // return -m_gyro.getRate();
+    // }
 
-    public double getEncoderPos() {
-        return encoder.get() * Constants.TICKS_TO_FEET;
-    }
+    // public double getEncoderPos() {
+    // return encoder.get() * Constants.TICKS_TO_FEET;
+    // }
 
-    public void resetEncoder() {
-        encoder.reset();
+    // public void resetEncoder() {
+    // encoder.reset();
 
-    }
+    // }
 }
