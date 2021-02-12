8e36fa53049de50cba648ad7691cb58a978d5b18
SatvikR <49799352+SatvikR@users.noreply.github.com>
2021-02-11 16:40:12 -0800

    did some stuff that was very coolsdjflsjfksdfjdsklfjklsdjfkdlsfkdlsfklsdjfk
    Co-authored-by: Luke Phillips <lcp.lukep@gmail.com>

--- src/main/java/frc/robot/subsystems/shooter/Tower.java
+++ src/main/java/frc/robot/subsystems/shooter/Tower.java
@@ -1,15 +1,27 @@
 package frc.robot.subsystems.shooter;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Tower extends SubsystemBase {
     
-    //  use WPI_TalonSRX
-    private WPI_TalonSRX towerMotor = new WPI_TalonSRX(Constants.TOWER_PORT);
+    private TalonSRX towerMotor = new TalonSRX(Constants.TOWER_PORT);
     
     public Tower() {
         
+    }
+
+    public void setZero() {
+        towerMotor.set(TalonSRXControlMode.PercentOutput, 0);
+    }
+    
+    public void setIn() {
+        towerMotor.set(TalonSRXControlMode.PercentOutput, Constants.TOWER_SPEED);
+    }
+
+    public void setReverse() {
+        towerMotor.set(TalonSRXControlMode.PercentOutput, -Constants.TOWER_SPEED);
     }
 }
