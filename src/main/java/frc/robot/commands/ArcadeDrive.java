7c55876f82d8b8fe4c8bbe2674c297148cf5fb33
Satvik Reddy <reddy.satvik@gmail.com>
2021-03-16 17:38:54 -0700

    added comments and thing and idk and someother thing I think
    Co-authored-by: sanniewan <sanniewan@users.noreply.github.com>

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
