f7449a0db194b93c43a0e0a33074a8eb7fc05c17
Roshi K <68883284+roshihi@users.noreply.github.com>
2021-03-18 17:34:43 -0700

    j

--- src/main/java/frc/robot/commands/ControlHopper.java
+++ src/main/java/frc/robot/commands/ControlHopper.java
@@ -7,36 +7,50 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.subsystems.shooter.Hopper;
 import frc.robot.subsystems.shooter.Shooter;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-public class TestRunFlywheel extends CommandBase {
-  /** Creates a new RunFlywheel. */
-  public TestRunFlywheel() {
+public class ControlHopper extends CommandBase {
+  private Button shootButton; 
+  private Button offButton;
+
+  public ControlHopper() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(Shooter.flywheel);
+    addRequirements(Hopper.tower, Hopper.funnel, Shooter.flywheel);
+    shootButton = new JoystickButton(Robot.joystick, Constants.SHOOT_CELL_BUTTON);
+    offButton = new JoystickButton(Robot.joystick, Constants.STOP_HOPP_AND_ROLL_BUTTON);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double speed = Robot.joystick.getRawAxis(Constants.TRIGGER_AXIS) * Constants.FLY_TRIGGER_CONSTANT;
-
-    Shooter.flywheel.setOnTrigger(speed);
+    if (shootButton.get()) {
+      Hopper.tower.setIn();
+      Hopper.funnel.setIn();
+    } else {
+      Hopper.tower.setZero();
+      Hopper.funnel.setZero();
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     Shooter.flywheel.setOff();
+    Hopper.funnel.setZero();
+    Hopper.tower.setZero();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return offButton.get();
   }
 }
--- src/main/java/frc/robot/commands/ShootCell.java
+++ src/main/java/frc/robot/commands/ShootCell.java
@@ -4,16 +4,16 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ShootCell extends ParallelCommandGroup {
+public class ShootCell extends SequentialCommandGroup {
   /** Creates a new ShootCell. */
   public ShootCell() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new TestRollTower(), new RunFlywheel());
+    addCommands(new RunFlywheel(), new ControlHopper());
   }
 }
