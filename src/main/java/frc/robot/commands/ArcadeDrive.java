983e260388477e5ef9b0622ca101c03435cbfc0c
Satvik Reddy <reddy.satvik@gmail.com>
2021-03-27 14:50:34 -0700

    commented out unused commands

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -31,7 +31,7 @@ public class ArcadeDrive extends CommandBase {
         Robot.drivetrain.setLeftMotor(left);
         Robot.drivetrain.setRightMotor(right);
 
-        Robot.drivetrain.checkMotorOutput();
+        //Robot.drivetrain.checkMotorOutput();
     }
 
     // Make this return true when this Command no longer needs to run execute()
