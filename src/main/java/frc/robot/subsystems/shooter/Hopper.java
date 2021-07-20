2d211c49a8acaf9d1ca9b337aa109314068c042b
Joseph N <jojorocketn@gmail.com>
2021-07-20 14:22:07 -0700

    Empty branch

--- src/main/java/frc/robot/subsystems/shooter/Hopper.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.shooter;
-
-public class Hopper {
-    public static final Funnel funnel = new Funnel();
-    public static final Tower tower = new Tower();
-}
--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.shooter;
-
-public class Shooter {
-    public static final Hood hood = new Hood();
-    public static final Flywheel flywheel = new Flywheel();
-}
